0edb70a77e23d8863cd499cd386694114da59075
Tolojanahary <79153294+AmazingTool777@users.noreply.github.com>
2024-11-18 15:03:01 +0300

    Eo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js"],
+  content: ["./public/**/*.{html,js}", "./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
